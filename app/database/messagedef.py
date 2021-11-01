dc806f2a3950051bb921cfb4598a67e1b24262c1
CharlieFunk1 <ydougroove@gmail.com>
2021-10-31 17:19:53 -0700

    second commit

---
+++ app/database/messagedef.py
@@ -0,0 +1,51 @@
+from sqlalchemy import *
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref, sessionmaker
+
+
+engine = create_engine('sqlite:////home/charliefunk/flask-projects/message/app/database/messagedef.db', echo=True)
+Base = declarative_base()
+ 
+########################################################################
+class Message(Base):
+    """"""
+    __tablename__ = "message"
+ 
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    userid = Column(String)
+    subject = Column(String)
+    content = Column(String)
+
+    #----------------------------------------------------------------------
+    def __init__(self, username, userid, subject, content):
+        """"""
+        self.username = username
+        self.userid = userid
+        self.subject = subject
+        self.content = content
+
+########################################################################
+class User(Base):
+    """"""
+    __tablename__ = "user"
+ 
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    password = Column(String)
+    email = Column(String)
+
+    #----------------------------------------------------------------------
+    def __init__(self, username, password, email):
+        """"""
+        self.username = username
+        self.password = password
+        self.email = email
+
+# create tables
+Base.metadata.create_all(engine)
+
+Database = sessionmaker(bind=engine)
+database = Database()
---
+++ config.py
@@ -0,0 +1,50 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = "B\xb2?.\xdf\x9f\xb7m\xf8\x8a%,\xf7\xc4\xfa\x91"
+
+    DB_NAME = "production-db"
+    DB_USERNAME = "admin"
+    DB_PASSWORD = "example"
+
+    IMAGE_UPLOADS = "/home/username/app/app/static/images/uploads"
+
+    SESSION_COOKIE_SECURE = True
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['ydougroove@gmail.com']
+
+class ProductionConfig(Config):
+    pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+    DB_NAME = "development-db"
+    DB_USERNAME = "admin"
+    DB_PASSWORD = "example"
+
+    IMAGE_UPLOADS = "/home/username/projects/my_app/app/static/images/uploads"
+
+    SESSION_COOKIE_SECURE = False
+
+class TestingConfig(Config):
+    TESTING = True
+
+    DB_NAME = "development-db"
+    DB_USERNAME = "admin"
+    DB_PASSWORD = "example"
+
+    SESSION_COOKIE_SECURE = False
+
